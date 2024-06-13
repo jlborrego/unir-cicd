3e4dde828af4e8aacdb834f16d450675b13532c4
jl.borrego <97025008+jlborrego@users.noreply.github.com>
2024-06-13 13:19:23 +0200

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,3 +1,11 @@
+import app
+import math
+
+
+class InvalidPermissions(Exception):
+    pass
+
+
 class Calculator:
     def add(self, x, y):
         self.check_types(x, y)
@@ -8,6 +16,9 @@ class Calculator:
         return x - y
 
     def multiply(self, x, y):
+        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
+            raise InvalidPermissions('User has no permissions')
+
         self.check_types(x, y)
         return x * y
 
@@ -21,10 +32,27 @@ class Calculator:
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
+        
+    def square(self, x):
+        self.check_type(x)
+        if x < 0:
+            raise TypeError("The square of a negative is not possible")
+        return math.sqrt(x)
+        
+    def logarithm(self, x):
+        self.check_type(x)
+        if x <= 0:
+            raise TypeError("The logarithm of a negative or zero is not possible")
+        return math.log10(x)
 
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
+            
+    def check_type(self, x):
+        if not isinstance(x, (int, float)):
+            raise TypeError("Parameters must be numbers")
+
 
 
 if __name__ == "__main__":  # pragma: no cover
